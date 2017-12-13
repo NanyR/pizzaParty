f6f34821eab3e8f9ef19412e93ddbd4b09036310
nanyr <nanyramirez0@gmail.com>
2017-12-13 08:11:35 -0500

    created react app with form to enter new customer

---
+++ pizzaParty/acme_pizza/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Customer from './components/Customer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Customer/>
+      </div>
+    );
+  }
+}
+
+export default App;
